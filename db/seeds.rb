04482fbf0e1f2259c6aaa58c6b002da72f8a6e09
kakeru.watanabe <jueda1104@gmail.com>
2018-10-22 13:16:49 +0000

    add Boostrap

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Task.create(status: "済", content: "Lesson9")
+Task.create(status: "済", content: "Lesson10")
